cabbda9d151fe72208294d433f2c56b55feaec27
Preston Ward <prestonsward@gmail.com>
2018-07-03 14:54:02 -0400

    Not working, but close.  Progress so far

--- rundmcmc/__main__.py
+++ rundmcmc/__main__.py
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import sys
 from rundmcmc.parse_config import read_basic_config
 =======
@@ -84,6 +85,37 @@ if __name__ == "__main__":
             print("county {}: {} ({})".format(county, info.split, info.contains))
 >>>>>>> Revert "Begin working on a faster single-flip contiguity check"
 
+=======
+from rundmcmc.Graph import Graph
+
+from rundmcmc.make_graph import (construct_graph)
+from rundmcmc.partition import Partition
+from rundmcmc.updaters import (Tally, boundary_nodes, cut_edges,
+                               cut_edges_by_part, exterior_boundaries,
+                               perimeters)
+from rundmcmc.updaters import polsby_popper_updater as polsby_popper
+from rundmcmc.updaters import votes_updaters
+
+def main():
+    G = Graph("./testData/PA_graph_with_data.json")
+    graph = construct_graph("./testData/PA_graph_with_data.json")
+    assignment = dict(zip(G.nodes(), G.node_properties('CD')))
+    print(assignment)
+
+    updaters = {
+        **votes_updaters(['VoteA', 'VoteB']),
+        'population': Tally('POP100', alias='population'),
+        'perimeters': perimeters,
+        'exterior_boundaries': exterior_boundaries,
+        'boundary_nodes': boundary_nodes,
+        'cut_edges': cut_edges,
+        'areas': Tally('ALAND10', alias='areas'),
+        'polsby_popper': polsby_popper,
+        'cut_edges_by_part': cut_edges_by_part
+    }
+
+    Partition(graph, assignment, updaters)
+>>>>>>> Not working, but close.  Progress so far
 
 if __name__ == "__main__":
     import sys
